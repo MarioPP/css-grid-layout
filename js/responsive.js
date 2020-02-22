b9e02adc32ede1a1164a12def60cdc9618381c1b
Mario Peña <devmariopp@gmail.com>
2020-02-22 13:15:41 -0400

    Subiendo archivos.

---
+++ js/responsive.js
@@ -0,0 +1,6 @@
+/* ----- RESPONSIVE ----- */
+function mobile()
+{
+  document.getElementById("overlay").classList.toggle("overlay-open");
+  document.getElementById("sidebarMenu").classList.toggle("mainSidebar-open");
+}
